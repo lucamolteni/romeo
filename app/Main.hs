3ab4c2904ef1c7bb129152c4a4cb212297fa5a88
Luca Molteni <volothamp@gmail.com>
2016-03-05 18:11:26 +0100

    Forever ask for a direction

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Lib
+import Control.Monad
+
 
 main :: IO ()
-main = someFunc
+main = forever echo
+
+
+echo = do putStrLn "Insert a direction:"
+          direction <- getLine
+          print direction
+          return ()
