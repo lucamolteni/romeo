09ed12075509dfabd0a8b4c36d95c2f12c900220
Luca Molteni <volothamp@gmail.com>
2016-03-06 10:20:27 +0100

    passing parameters in loop

--- app/Main.hs
+++ app/Main.hs
@@ -8,19 +8,18 @@ import           Lib
 main :: IO ()
 main = do putStrLn "Insert Player Name:"
           playerName <- getLine
-          let newPlayer = Player playerName 0 0
-          loop [newPlayer]
+          let player1 = Player playerName 0 0
+          loop [] player1
 
-loop :: [Player] -> IO ()
-loop initialState = do  let player = last initialState
-                        putStrLn $ name player ++ ", insert a direction:"
-                        input <- getLine
-                        let direction = parseDirection input
-                        let newPos = fmap (move player) direction
-                        let allPos = initialState ++ maybeToList newPos
-                        mapM_ print allPos
-                        loop allPos
-
+loop :: [Player] -> Player -> IO ()
+loop initialState player1 = do  putStrLn $ name player1 ++ ", insert a direction:"
+                                input <- getLine
+                                let direction = parseDirection input
+                                let maybeNewPos =  fmap (move player1) direction
+                                let loggedEvent = maybeToList maybeNewPos
+                                let log = initialState ++ loggedEvent
+                                mapM_ print log
+                                loop log (lastWithDefault loggedEvent player1)
 
 lastWithDefault :: [a] -> a -> a
 lastWithDefault list def = if null list then def else last list
