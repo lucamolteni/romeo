9b6e26e94f4ae3ac09cbebbc1760011dbdae25af
Luca Molteni <volothamp@gmail.com>
2016-03-06 10:42:15 +0100

    Printing the log every time a player moves

--- app/Main.hs
+++ app/Main.hs
@@ -7,18 +7,22 @@ import           Lib
 
 main :: IO ()
 main = do player1 <- askForPlayerName
-          loop [] player1
+          player2 <- askForPlayerName
+          loop [] player1 player2
 
 askForPlayerName :: IO Player
 askForPlayerName = do putStrLn "Insert Player Name:"
                       playerName <- getLine
                       return $ Player playerName 0 0
 
-loop :: [Player] -> Player -> IO ()
-loop initialState player1 = do  newPlayer1 <- movePlayerLoop player1
-                                let log = initialState ++ [newPlayer1]
-                                mapM_ print log
-                                loop log newPlayer1
+loop :: [Player] -> Player -> Player -> IO ()
+loop initialState player1 player2 = do  newPlayer1 <- movePlayerLoop player1
+                                        let log1 = initialState ++ [newPlayer1]
+                                        mapM_ print log1
+                                        newPlayer2 <- movePlayerLoop player2
+                                        let log2 = log1 ++ [newPlayer2]
+                                        mapM_ print log2
+                                        loop log2 newPlayer1 newPlayer2
 
 movePlayerLoop :: Player -> IO Player
 movePlayerLoop player = do putStrLn $ name player ++ ", insert a direction:"
@@ -28,7 +32,3 @@ movePlayerLoop player = do putStrLn $ name player ++ ", insert a direction:"
 movePlayer :: Player -> String -> Player
 movePlayer p input = fromMaybe p newPlayer1
     where newPlayer1 = fmap (move p) (parseDirection input)
-
-
-lastWithDefault :: [a] -> a -> a
-lastWithDefault list def = if null list then def else last list
